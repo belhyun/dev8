3c0846974aaa99d081790b0f60a4e71df5da6594
베니(belhyun) <belhyun@coupang.com>
2017-02-26 00:16:03 +0900

    tt

--- api/handlers/images/images.js
+++ api/handlers/images/images.js
@@ -1,22 +1,19 @@
 import { projects } from './imagesRepository';
 import Behance from './Behance';
-import _ from 'underscore';
-let log = console.log.bind(console);
+var keys = {
+    "client_id": "rteQQ9wDq1mF84wiKzFWssSTkWzK2UQQ",
+    "client_secret":""
+};
+var behance = new Behance(keys);
 
 exports.images = function(req, res, next) {
-    var keys = {
-        "client_id": "rteQQ9wDq1mF84wiKzFWssSTkWzK2UQQ",
-        "client_secret":""
-    };
-    var behance = new Behance(keys);
-    behance.get('projects', projects("dog"), function(result){
+    behance.get('projects', projects(req.query.query), function(resp){
+        res.send(resp);
+    });
+};
 
-        _.map(result, function(each) {
-            var callback = function(result) {
-                log(result);
-            };
-            behance.get("projects/"+each.id, callback);
-
-        });
+exports.image = function(req, res, next) {
+    behance.get("projects/" + req.params.id, function(project) {
+        res.send(project);
     });
 };
--- api/routes.js
+++ api/routes.js
@@ -2,4 +2,7 @@ import IMAGE_HANDLER from './handlers/images/images.js';
 
 module.exports = function(app) {
     app.get('/images', IMAGE_HANDLER.images);
+    app.get('/image/:id', IMAGE_HANDLER.image);
 };
+
+
